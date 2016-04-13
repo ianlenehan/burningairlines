e512ea10e7414c576a8d223c03b8e6a8c37c8d38
“Ian <ianlenehan@gmail.com>
2016-04-13 23:25:05 +1000

    further work on search feature incl adding moment.js

--- app/assets/javascripts/views/SearchFlightView.js
+++ app/assets/javascripts/views/SearchFlightView.js
@@ -4,11 +4,18 @@ app.SearchFlightView = Backbone.View.extend({
 
   events: {
     'click #search_save' : 'searchFlights',
-    'click .result_row' : 'viewFlight'
-    // 'click #'
+    'click .result_row' : 'viewFlight',
+    'keypress input' : 'checkForEnter'
   },
 
   tagName: 'div',
+
+  checkForEnter: function (e) {
+    app.ENTER_KEY = 13;
+    if (e.which === app.ENTER_KEY) {
+      this.searchFlights();
+    }
+  },
 
   searchFlights: function () {
     var origin = this.$el.find('#origin').val();
@@ -36,17 +43,17 @@ app.SearchFlightView = Backbone.View.extend({
         for (var i = 0; i < matchingFlights.length; i++) {
           var origin_result = matchingFlights[i].attributes.origin;
           var destination_result = matchingFlights[i].attributes.destination;
-          var date_result = matchingFlights[i].attributes.date;
+          var date_result = moment(matchingFlights[i].attributes.date).format('MMMM Do YYYY, h:mma');
           var plane_result = matchingFlights[i].attributes.plane_id;
           var flight_result = matchingFlights[i].attributes.id;
           var rowID = 'result_row_' + flight_result;
           console.log(rowID);
           $('#flights_list').append('<tr class="result_row" id="result_row_' + flight_result + '" data-id="' + flight_result + '"></tr>');
-          $('#' + rowID).append('<td>' + date_result + '</td>');
           $('#' + rowID).append('<td>' + flight_result + '</td>');
           $('#' + rowID).append('<td>' + origin_result + '</td>');
           $('#' + rowID).append('<td>' + destination_result + '</td>');
           $('#' + rowID).append('<td>' + plane_result + '</td>');
+          $('#' + rowID).append('<td>' + date_result + '</td>');
         }
     });
   },
