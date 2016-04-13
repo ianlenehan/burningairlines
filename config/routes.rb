cc799a4f67256ec2994d3d56047738b8f750910e
Carmen Minikus <carmenminikus@gmail.com>
2016-04-14 08:41:38 +1000

    changed homepage to login and fixed login signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  root :to => 'flights#home'
+  root :to => 'users#new'
 
   resources :users
   resources :reservations
   resources :flights
   resources :planes
 
-  get '/signup' => 'users#new'
-
-  get '/login' => 'session#new'
-  post '/login' => 'session#create'
-  delete '/login' => 'session#destroy'
+get '/search' => 'flights#home'
+get '/login' => 'session#new'
+post '/login' => 'session#create'
+delete '/login' => 'session#destroy'
 
 end
