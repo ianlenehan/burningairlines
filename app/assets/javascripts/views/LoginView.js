a52505177960398050f7527bcdb8b6b6beb9b822
Carmen Minikus <carmenminikus@gmail.com>
2016-04-14 07:40:07 +1000

    fixing logins

--- app/assets/javascripts/views/LoginView.js
+++ app/assets/javascripts/views/LoginView.js
@@ -1,20 +1,20 @@
-var app = app || {}
-
-app.LoginView = Backbone.View.extend({
-
-  el: '#main',
-
-  render: function () {
-    var loginTemplate = _.template($('#flightViewTemplate').text());
-    var loginAttr = this.model.attributes;
-    loginAttr.plane = this.model.plane.attributes;
-
-    this.$el.html( loginTemplate(loginAttr) );
-
-    this.listenTo(this.model.user, 'change', function () { //when the plane loads it needs to show plane desc.
-      var planeDescriptionTemplate = _.template($('#planeDescriptionTemplate').text());
-      var planeDescription = planeDescriptionTemplate(this.model.plane.attributes);
-      this.$el.find('#planeDescription').text(planeDescription);
-    })
-  }
-})
+// var app = app || {}
+//
+// app.LoginView = Backbone.View.extend({
+//
+//   el: '#main',
+//
+//   render: function () {
+//     var loginTemplate = _.template($('#flightViewTemplate').text());
+//     var loginAttr = this.model.attributes;
+//     loginAttr.plane = this.model.plane.attributes;
+//
+//     this.$el.html( loginTemplate(loginAttr) );
+//
+//     this.listenTo(this.model.user, 'change', function () {
+//       var planeDescriptionTemplate = _.template($('#planeDescriptionTemplate').text());
+//       var planeDescription = planeDescriptionTemplate(this.model.plane.attributes);
+//       this.$el.find('#planeDescription').text(planeDescription);
+//     })
+//   }
+// })
