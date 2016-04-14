8cd60a86079595b4ebf60ec74eb52ea5d3a421df
“Ian <ianlenehan@gmail.com>
2016-04-14 14:48:36 +1000

    users page updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find params[:id]
+    @reservations = Reservation.where(user_id: @user.id)
   end
 
   # GET /users/new
