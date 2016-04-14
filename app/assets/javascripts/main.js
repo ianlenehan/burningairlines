f3911e48c8fdce80eab2e3bd17813ddcf3e23632
Carmen Minikus <carmenminikus@gmail.com>
2016-04-14 10:41:14 +1000

    search function for flight ID in nav bar added

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,4 +5,19 @@ $(document).ready(function () {
   app.router = new app.AppRouter();
   Backbone.history.start();
 
+  var searchFlightsById = function(event) {
+    var flightId = $('.searchFlightById').val();
+    event.preventDefault();
+    var newURL = "/flights/" + flightId;
+    app.router.navigate(newURL, true);
+
+  };
+
+  $('#navSearch').on('click', searchFlightsById);
+  $('.searchFlightById').on('keydown', function(event){
+    if (event.keyCode === 13) {
+      searchFlightsById();
+    }
+  });
+
 });
